618eaad6a9524cfef18ef993eab6e26f5984b03b
houjinyun <houjinyun@gmail.com>
2018-12-21 11:41:47 +0800

    完善404页面，提醒版本升级

--- library/src/main/java/com/hm/iou/router/PageNotFoundActivity.java
+++ library/src/main/java/com/hm/iou/router/PageNotFoundActivity.java
@@ -1,7 +1,13 @@
 package com.hm.iou.router;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by hjy on 2018/5/22.
@@ -13,5 +19,25 @@ public class PageNotFoundActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.router_activity_page_not_found);
+
+        findViewById(R.id.btn_check_version).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWebBrowser(PageNotFoundActivity.this, "https://a.app.qq.com/o/simple.jsp?pkgname=com.hm.iou");
+            }
+        });
     }
+
+    public static void openWebBrowser(Context context, String url) {
+        final Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        if (intent.resolveActivity(context.getPackageManager()) != null) {
+            final ComponentName componentName = intent.resolveActivity(context.getPackageManager());
+            context.startActivity(Intent.createChooser(intent, "请选择浏览器"));
+        } else {
+            Toast.makeText(context.getApplicationContext(), "请下载浏览器", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
